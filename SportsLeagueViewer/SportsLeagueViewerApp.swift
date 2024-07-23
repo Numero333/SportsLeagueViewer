3b6e1ed0bb3a0e6379d5fe3e5c1e720d011560bb
xNumero3 <notnumero3@gmail.com>
2024-07-23 17:29:49 +0200

    Initial Commit

---
+++ SportsLeagueViewer/SportsLeagueViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsLeagueViewerApp.swift
+//  SportsLeagueViewer
+//
+//  Created by François-Xavier on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SportsLeagueViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
